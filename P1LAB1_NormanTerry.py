0b99caf701b5816fb885c4caee2e28de592c6ff4
Blizzybot <normant7386@student.faytechcc.edu>
2026-02-22 19:42:58 -0500

    Add files via upload

---
+++ P1LAB1_NormanTerry.py
@@ -0,0 +1,9 @@
+# Terry Norman
+# Feb 22, 2026
+# P1LAB1
+# This program will ask the user for their name and then greet them.
+# Get the user's first name
+first_name = input("Please enter your first name: ")
+# Get the user's last name
+last_name = input("Please enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110!")
